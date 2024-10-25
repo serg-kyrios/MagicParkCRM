c47d356539694806bf6a36b2ff84f97414157b50
Sergey KYRYLIUK <sergkyrios@gmail.com>
2024-10-25 15:50:25 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            {/* <img src={logo} className='App-logo' alt='logo' /> */}
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
